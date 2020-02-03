c936d4cf833d704754020259cbc15aa43c9fe952
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2020-02-03 15:55:14 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -11,11 +11,12 @@
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            //If you have default property values in your metadata .json, we recommend commenting out this code and calling redraw() from onCustomWidgetAfterUpdate()
+            this._firstConnection = true;
             this.redraw();
         }
 
@@ -31,8 +32,9 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-            //If you have default property values in your metadata .json, we recommend uncommenting this and calling redraw() from here, instead of connectedCallback()
-            //this.redraw();
+            if (this._firstConnection){
+                this.redraw();
+            }
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
@@ -41,12 +43,15 @@
 
         
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
         /*
         onCustomWidgetResize(width, height){
+        
         }
         */
 
-        redraw(){}
+       redraw(
+        )
     });
 })();
